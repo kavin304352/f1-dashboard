5e08d4108bd1169d833a98319ece80df11658053
Kavinllan43 <kavinilan2001@gmail.com>
2025-06-10 18:36:46 -0400

    Completed functionality for track times

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,9 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from f1_data import get_fastest_laps
+from fastf1 import get_event_schedule
+import pandas as pd
+import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -10,10 +13,26 @@ CORS(app)
 @app.route("/api/fastest-laps/<int:year>/<string:gp>")
 def fastest_laps(year, gp):
     try:
+        #get the data of the fastest lap
         data = get_fastest_laps(year, gp)
         return jsonify(data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route("/api/tracks")
+def get_available_tracks():
+    current_year = datetime.datetime.now().year
+    schedule = get_event_schedule(current_year)
+
+    # Only up to date grand prix's
+    today = datetime.datetime.now().date()
+    past = schedule[schedule['EventDate'] < pd.Timestamp(today)]
+
+    
+    result = [
+        {"name": row["EventName"], "year": current_year}
+        for _, row in past.iterrows()
+    ]
+    return jsonify(result)
 if __name__ == "__main__":
     app.run(debug = True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import TrackPage from './TrackPage';
 
 function App() {
-  const [laps, setLaps] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/api/fastest-laps/2025/Monaco")
-      .then((res) => {
-        if (!res.ok) throw new Error("Failed to fetch data");
-        return res.json();
-      })
-      .then((data) => setLaps(data))
-      .catch((err) => setError(err.message));
-  }, []);
-
   return (
-    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>Fastest Laps - Monaco 2025</h1>
-
-      {error && <p style={{ color: "red" }}>Error: {error}</p>}
-
-      {laps.length === 0 && !error && <p>Loading...</p>}
-
-      <ul>
-        {laps.map((lap, index) => (
-          <li key={index}>
-            <strong>{lap.Driver}</strong>: {lap.LapTime.replace('0 days ', '')}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/track/:trackName" element={<TrackPage />} />
+    </Routes>
   );
 }
 
